7eaf86ae4cd7f43a0d9fb400b6fd7e98403915c5
Umme Hani <uh.snigdha@gmail.com>
2025-03-19 22:45:20 +0600

    versioned database schema

---
+++ server/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE `school2`;
+
+CREATE TABLE `students` (
+  `ID` int NOT NULL,
+  `RollNumber` int NOT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `RollNumber` (`RollNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
