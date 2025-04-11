b47342c9d33e1663122f4054b9ecf4ac65437b83
Umme Hani <uh.snigdha@gmail.com>
2025-04-11 23:12:05 +0600

    - installed 'shadcn' components. - converted students table into shadcn data table.

--- school/src/app/page.js
+++ school/src/app/page.js
@@ -1,36 +1,138 @@
 'use client';
+import { useEffect, useState } from "react";
 
 import Link from "next/link";
-import studentList from "./students";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+ 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
+
+import { 
+  DropdownMenu, 
+  DropdownMenuContent, 
+  DropdownMenuItem, 
+  DropdownMenuLabel, 
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 
 export default function Home() {
 
-  let students = [];
+  console.log("Component initializing...")
 
-  for (let i = 0; i < studentList.length; i++) {
+  const [students, setStudents] = useState([]);
 
-    students.push((<tr key={studentList[i].id} onClick={() => {
-      alert("You click me!");
-    }}>
-      <td>{studentList[i].name}</td>
-      <td>{studentList[i].clzz}</td>
-      <td>{studentList[i].roll}</td>
-      <td><Link href={'/student/' + studentList[i].id}>details</Link></td>
-    </tr>))
-  }
+  const table = useReactTable({
+    data: students,
+    columns: [
+      {
+        accessorKey: 'ID',
+        header: () => <span>ID</span>
+      },
+      {
+        accessorKey: 'RollNumber',
+        header: () => <span>Roll Number</span>
+      },
+      {
+        accessorKey: 'FirstName',
+        header: () => <span>First Name</span>
+      },
+      {
+        accessorKey: 'LastName',
+        header: () => <span>Last Name</span>
+      },
+      {
+        id: 'actions',
+        header: () => <span>Actions</span>,
+        cell: ({ row }) => (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <MoreHorizontal className="h-4 w-4" />
+                <span className="sr-only">Open menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>
+                <Link href={'/student/' + row.original.id}>View</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={'/student/edit/' + row.original.id}>Edit</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={'/student/delete/' + row.original.id}>Delete</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ),
+      }
+    ],
+    getCoreRowModel: getCoreRowModel(),
+  });
 
+  const url = `http://localhost:5000/students`
+  const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+  };
+
+  useEffect(() => {
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log('data:', data)
+
+        setStudents(data);
+      })
+      .catch(error => console.log('Submit error:', error));
+  }, []);
+ 
   return (
     <div>
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Clazz</th>
-            <th>Roll</th>
-          </tr>
-          {students}
-        </tbody>
-      </table>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map(headerGroup => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <TableHead key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : (
+                    <div className="flex items-center justify-between">
+                      <span>
+                        {flexRender(header.column.columnDef.header, header.getContext())}
+                      </span>
+                    </div>
+                  )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.map(row => (
+            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+              {row.getVisibleCells().map(cell => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
