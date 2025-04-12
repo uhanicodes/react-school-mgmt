bc5926887a9997e6d636977a1926656bed9d6ad7
Umme Hani <uh.snigdha@gmail.com>
2025-04-12 22:39:52 +0600

    introduced new student creation form.

---
+++ school/src/app/student-form/page.js
@@ -0,0 +1,111 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import React from "react";
+import { useState } from "react";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+export default function studentForm() {
+
+  let submitFrom = (data) => {
+
+    console.log("data", data);
+
+    const url = 'http://localhost:5000/students'
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+
+    fetch(url, requestOptions)
+      .then(response => console.log('Submitted successfully'))
+      .catch(error => console.log('Form submit error', error))
+  }
+
+  const form = useForm();
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(submitFrom)} className="space-y-8">
+        <FormField 
+          control={form.control} 
+          name='id'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ID</FormLabel>
+              <FormControl>
+                <Input placeholder="ID" {...field} />
+              </FormControl>
+              <FormDescription>
+                Put ID
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='rollNumber'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Roll</FormLabel>
+              <FormControl>
+                <Input placeholder="Roll" {...field} />
+              </FormControl>
+              <FormDescription>Write your roll number</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='firstName'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>First Name</FormLabel>
+              <FormControl>
+                <Input placeholder="First Name" {...field} />
+              </FormControl>
+              <FormDescription>Write your first name</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='lastName'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Last Name" {...field} />
+              </FormControl>
+              <FormDescription>Write your last name</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />                
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
